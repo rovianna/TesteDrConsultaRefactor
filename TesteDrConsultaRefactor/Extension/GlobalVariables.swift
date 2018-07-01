ffb7ba60eafe7949592a81f3e85ffad3c4cd9370
Rodrigo Vianna <rodrigogvianna@outlook.com>
2018-07-01 09:44:26 -0300

    The First Commit, just to add some organization

---
+++ TesteDrConsultaRefactor/Extension/GlobalVariables.swift
@@ -0,0 +1,11 @@
+//
+//  GlobalVariables.swift
+//  TesteDrConsultaRefactor
+//
+//  Created by Rodrigo Vianna on 01/07/18.
+//  Copyright © 2018 Rodrigo Vianna. All rights reserved.
+//
+
+import Foundation
+
+//Just Kidding, won't use it! <3
